70a1b27e8be0560fb89fbd7e61af187d24de5dee
angrykeo@gmail.com <angrykeo@gmail.com>
2023-11-19 13:18:22 +0300

    Day3

--- service/service/settings.py
+++ service/service/settings.py
@@ -126,3 +126,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {'class':'logging.StreamHandler'}
+    },
+    'loggers':{
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    }
+}
